1a800e495c117925a299a7408c0fb370e4637b09
ebravofm <ebravofm@gmail.com>
2019-05-05 16:13:23 -0400

    Added gExcelFile

--- gpandas/__init__.py
+++ gpandas/__init__.py
@@ -3,6 +3,19 @@ import pandas as pd
 from io import BytesIO
 
 def read_gexcel(id, **pd_read_excel_args):
+    '''
+    Read Google Sheets worksheet as pandas dataframe. id argument is the id parameter in workbook url.
+    '''
+
+    excel_data = gExcelFile(id)
+    
+    return pd.read_excel(excel_data, **pd_read_excel_args)
+
+
+def gExcelFile(id):
+    '''
+    Read Google Sheets workbook as pandas ExcelFile. id argument is the id parameter in workbook url.
+    '''
 
     if 'id=' in id:
         id = id.split('id=')[1]
@@ -11,4 +24,4 @@ def read_gexcel(id, **pd_read_excel_args):
     r = requests.get(dl) 
     excel_data = pd.ExcelFile(BytesIO(r.content))
 
-    return pd.read_excel(excel_data, **pd_read_excel_args)
+    return excel_data
