267307bca4febdd646a34c86323da277f8651c38
ebravofm <ebravofm@gmail.com>
2019-05-16 16:38:45 -0400

    details

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='gpandas',
+    version = '0.1',
     url='https://github.com/ebravofm/gpandas',
+    keywords = 'press news scraper news extractor twitter mysql information retrieval',
     author='Emilio Bravo',
     author_email='ebravofm@gmail.com',
     packages=['gpandas'],
     install_requires=['pandas', 'requests', 'xlrd'],
     version='0.1',
-    license='MIT',
+    license = 'Apache License 2.0',
     description='Simple tool for quickly reading a public google spreasheet. Does not require Google OAuth.',
 )
